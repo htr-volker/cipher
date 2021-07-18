e1eb85ef3c7372b9bc9385d7808882c01ec73f48
htr-volker <htr.volker@gmail.com>
2021-07-18 22:30:09 +0100

    fixing install script

--- application/functions.py
+++ application/functions.py
@@ -1,5 +1,4 @@
 import random
-import os
 
 def encode(string):
     output = ''
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt install python3 python3-pip
+sudo apt update
+sudo apt install python3 python3-pip -y
 
 python3 -m pip install -U pip
 python3 -m pip install -r requirements.txt
